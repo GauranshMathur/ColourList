3fc7c21ff30ca2d75500af0b9d0767f90833301b
GauranshMathur <40708506+GauranshMathur@users.noreply.github.com>
2018-06-30 12:03:56 +0800

    Input Comments

--- ColourCode.py
+++ ColourCode.py
@@ -1,5 +1,15 @@
 def getCSSCodeForColourHex(Hex):
+	'''
+	DOCSTRING : returns the css code for background with Hex val
+	INPUT : hex value
+	OUTPUT : CSS code
+	'''
 	return ".mybgcolor {background-color:%s;}" %(Hex)
 
 def getHTMLCodeForColourHex(Hex):
+	'''
+	DOCSTRING : returns the HTML code for background with Hex val
+	INPUT : hex value
+	OUTPUT : HTML code
+	'''
 	return "<div style=\"background-color:%s\">Div content here</div>" %(Hex)
--- CreateFile.py
+++ CreateFile.py
@@ -1,4 +1,9 @@
 def fileCreateIfNotExist():
+	'''
+	DOCSTRING : Checks if the files exist and if are empty or not.
+	INPUT : 
+	OUTPUT : 
+	'''
 		with open('ColourMainList',mode='w+') as cFile:
 			cFile.seek(0) 
 			first_char = cFile.read(1)  
@@ -22,30 +27,34 @@ def fileCreateIfNotExist():
 
          
 def addValToMainList():
+	'''
+	DOCSTRING : Writes values to the file
+	INPUT : 
+	OUTPUT : 
+	'''
 	with open('ColourMainList',mode='w') as cFile:
 		cFile.write("Red : #ff0000\nOrange : #ffa500\nYellow : #ffff00\n"
 		"Green : #008000\nBlue : #0000ff\nIndigo : #4b0082\nViolet : #ee82ee")
 	cFile.close()
 
 def addValToSecondaryList():
+	'''
+	DOCSTRING : Writes values to the file
+	INPUT : 
+	OUTPUT : 
+	'''
 	with open('ColourSecondaryList',mode='w') as cFile:
 		cFile.write("Purple : #800080\nMagenta : #ff00ff\nCyan : #00ffff\n"
 		"White : #ffffff\nBlack : #000000\nGrey : #808080\nPink : #ffc0cb")
 	cFile.close()
 
 def addValToTertiaryList():
+	'''
+	DOCSTRING : Writes values to the file
+	INPUT : 
+	OUTPUT : 
+	'''
 	with open('ColourTertiaryList',mode='w') as cFile:
 		cFile.write("Amber : #ffbf00\nVermilion : #e34234\nTeal : #008080\n"
 		"Chartreuse : #7fff00\nScarlet : #ff2400\nAubergine : #614051\nTurquoise : #40e0d0")
 	cFile.close()
-
-
-
-
-
-
-
-#.lstrip to strip a character
-fileCreateIfNotExist()
-with open('ColourTertiaryList', mode='r') as cFile:
-	print(cFile.readlines())
